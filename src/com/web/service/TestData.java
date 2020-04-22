d18f9907ef133cec20800f83e347c2ec5f3e8a42
SepJa2.7 <jalayersepehr@gmail.com>
2020-04-22 10:06:52 -0400

    added preDemo testData

--- src/com/web/service/TestData.java
+++ src/com/web/service/TestData.java
@@ -34,68 +34,26 @@ public class TestData {
 		WebInterface QUEobj = quebecService.getPort(WebInterface.class);
 		WebInterface SHEobj = sherbrookService.getPort(WebInterface.class);
 
-		System.out.println("Adding Data");
+		System.out.println("PreDemo TestCases");
 		System.out.println("*********************************************************");
 
-		SHEobj.addEvent("SHEE110620", CONFERENCES, 1);
-		SHEobj.addEvent("SHEE080620", CONFERENCES, 1);
+		System.out.println("Logged in as MTLM3456 MANAGER:");
+		System.out.println(MTLobj.addEvent("MTLA080820", CONFERENCES, 2));
+		System.out.println(MTLobj.addEvent("MTLM110820", SEMINARS, 1));
+		System.out.println(MTLobj.addEvent("MTLM120820", SEMINARS, 1));
 
-		MTLobj.addEvent("MTLA090620", CONFERENCES, 2);
-		MTLobj.addEvent("MTLA080620", TRADE_SHOWS, 2);
-		MTLobj.addEvent("MTLE230620", SEMINARS, 1);
-		MTLobj.addEvent("MTLA150620", TRADE_SHOWS, 12);
+		System.out.println("Logged in as SHEM9000 MANAGER:");
+		System.out.println(SHEobj.addEvent("SHEE080820", TRADE_SHOWS, 1));
 
-		QUEobj.bookEvent("QUEC1234", "SHEE080620", CONFERENCES);
-		QUEobj.bookEvent("QUEC1234", "MTLA090620", CONFERENCES);
-		QUEobj.bookEvent("QUEC1234", "MTLA080620", TRADE_SHOWS);
+		System.out.println("Logged in as QUEM9000 MANAGER:");
+		System.out.println(QUEobj.addEvent("QUEA250820", SEMINARS, 1));
+		System.out.println(QUEobj.addEvent("QUEE150820", TRADE_SHOWS, 1));
 
-		QUEobj.bookEvent("QUEC4114", "MTLA080620", TRADE_SHOWS);
+		System.out.println("Logged in as SHEC1234 CUSTOMER:");
+		System.out.println(SHEobj.bookEvent("SHEC1234", "MTLA080820", CONFERENCES));
+		System.out.println(SHEobj.bookEvent("SHEC1234", "MTLM110820", SEMINARS));
+		System.out.println(SHEobj.bookEvent("SHEC1234", "QUEA250820", SEMINARS));
+		System.out.println(SHEobj.bookEvent("SHEC1234", "SHEE080820", TRADE_SHOWS));
 
-		System.out.println();
-		System.out.println("Testing Started");
-		System.out.println("*********************************************************");
-
-		System.out.println("Test1");
-		System.out.println("Quebec Manager listEventAvailability:");
-		System.out.println(QUEobj.listEventAvailability(CONFERENCES));
-		System.out.println(QUEobj.listEventAvailability(SEMINARS));
-		System.out.println(QUEobj.listEventAvailability(TRADE_SHOWS));
-		System.out.println("*********************************************************");
-
-		System.out.println("Test2");
-		System.out.println("QUEC1234 bookEvent:");
-		System.out.println(QUEobj.bookEvent("QUEC1234", "SHEE110620", CONFERENCES));
-		System.out.println("QUEC1234 bookEvent:");
-		System.out.println(QUEobj.bookEvent("QUEC1234", "MTLE230620", SEMINARS));
-		System.out.println("*********************************************************");
-
-		System.out.println("Test3");
-		System.out.println("QUEC1234 cancelEvent:");
-		System.out.println(QUEobj.cancelEvent("QUEC1234", "MTLA090620", CONFERENCES));
-		System.out.println("*********************************************************");
-
-		System.out.println("Test4");
-		System.out.println("SHEC2345 bookEvent:");
-		System.out.println(SHEobj.bookEvent("SHEC2345", "SHEE080620", CONFERENCES));
-		System.out.println("*********************************************************");
-
-		System.out.println("Test5");
-		System.out.println("Montreal Manager removeEvent:");
-		System.out.println(MTLobj.removeEvent("MTLA080620", TRADE_SHOWS));
-		System.out.println("*********************************************************");
-
-		System.out.println("Test6");
-		System.out.println("SHEC2345 Booking Schedule:");
-		System.out.println(SHEobj.getBookingSchedule("SHEC2345"));
-		System.out.println("QUEC1234 Booking Schedule:");
-		System.out.println(QUEobj.getBookingSchedule("QUEC1234"));
-		System.out.println("*********************************************************");
-
-		System.out.println("Test7");
-		System.out.println("Sherebrook Manager removeEvent:");
-		System.out.println(SHEobj.listEventAvailability(CONFERENCES));
-		System.out.println(SHEobj.listEventAvailability(SEMINARS));
-		System.out.println(SHEobj.listEventAvailability(TRADE_SHOWS));
-		System.out.println("*********************************************************");
 	}
 }
